d07eca7cc3c6e082a3de92ef07dc1a172993ed8f
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-07-03 20:28:02 -0400

    refactor employee questions

--- index.js
+++ index.js
@@ -1,90 +1,66 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const teamArr = []
 
-// const employeeQ = [
-//   {
-//     type: "list",
-//     name: "role",
-//     message: "What type of employee is this?",
-//     choices: ["Engineer", "Intern"],
-//   },
-//   {
-//     type: "input",
-//     name: "name",
-//     messgage: "What is the name of the employee?",
-//     validate: (employeeName) => {
-//       if (employeeName) {
-//         return true;
-//       } else {
-//         console.log("Please provide employee name.");
-//         return false;
-//       }
-//     },
-//   },
-//   {
-//     type: "input",
-//     name: "email",
-//     message: "Input the employees email?",
-//     validate: (employeeEmail) => {
-//       if (employeeEmail) {
-//         return true;
-//       } else {
-//         console.log("Please provide the employees email.");
-//         return false;
-//       }
-//     },
-//   },
-// ];
+function init(){
+  employeeQ()
+}
 
-const managerQ = (data) => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      messgage: "What is the name of the employee?",
-      validate: (employeeName) => {
-        if (employeeName) {
-          return true;
-        } else {
-          console.log("Please provide employee name.");
-          return false;
-        }
-      },
+ const employeeQ =  () => [
+  inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of the employee?",
+    validate: (employeeName) => {
+      if (employeeName) {
+        return true;
+      } else {
+        console.log("Please provide employee name.");
+        return false;
+      }
+  },
+},
+  {
+    type: "list",
+    name: "role",
+    message: "What type of employee is this?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  
+  {
+    type: "input",
+    name: "email",
+    message: "Input the employees email?",
+    validate: (employeeEmail) => {
+      if (employeeEmail) {
+        return true;
+      } else {
+        console.log("Please provide the employees email.");
+        return false;
+      }
     },
-    {
-      type: "input",
-      name: "email",
-      message: "Input the employees email?",
-      validate: (employeeEmail) => {
-        if (employeeEmail) {
-          return true;
-        } else {
-          console.log("Please provide the employees email.");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the managers ID?",
-    },
-    {
-      type: "input",
-      name: "office",
-      message: "What is the managers office number?",
-    },
-  ]);
-};
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employees ID?",
+  },
+]).then(function({name, role, email, id}) {
+  let employeeInfo = ""
+  if (role === "Manager"){
+    employeeInfo = " Office number"
+  }else if (role === "Engineer") {
+    employeeInfo = "Github"
+  }else if (role === "Intern"){
+    employeeInfo = "University"
+  }
 
-// function init() {
-//   inquirer
-//     .prompt(employeeQ)
-//     // .then(managerQ)
-//     .then(function (userInput) {
-//       console.log(userInput);
-//       //   writeToFile("README.md", generateMarkdown(userInput));
-//     });
-// }
+  inquirer.prompt({
+    message: `Enter employees ${ employeeInfo }`,
+    name: "employeeInfo"
+  })
+})
+]
 
-// init();
+init()
